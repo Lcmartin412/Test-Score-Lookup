df5f8496e422c5682bed04ea76f41534d36a00ba
lcmartin412 <lcmartin412@gmail.com>
2020-04-07 21:05:25 -0400

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace TestScoreLookupandUpdateLeviMartin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentNumberListbox = new System.Windows.Forms.ListBox();
+            this.selectionGroupbox = new System.Windows.Forms.GroupBox();
+            this.finalScoreRadioButton = new System.Windows.Forms.RadioButton();
+            this.findMidtermRadiobutton = new System.Windows.Forms.RadioButton();
+            this.averageFinalsRadiobutton = new System.Windows.Forms.RadioButton();
+            this.averageMidtermsRadioButton = new System.Windows.Forms.RadioButton();
+            this.averageaAllTestsRadioButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.midtermLabel = new System.Windows.Forms.Label();
+            this.finalLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.midtermTextbox = new System.Windows.Forms.TextBox();
+            this.finalTextbox = new System.Windows.Forms.TextBox();
+            this.updateMidtermButton = new System.Windows.Forms.Button();
+            this.updateFinalButton = new System.Windows.Forms.Button();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.selectionGroupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // studentNumberListbox
+            // 
+            this.studentNumberListbox.FormattingEnabled = true;
+            this.studentNumberListbox.Location = new System.Drawing.Point(230, 68);
+            this.studentNumberListbox.Name = "studentNumberListbox";
+            this.studentNumberListbox.Size = new System.Drawing.Size(88, 30);
+            this.studentNumberListbox.TabIndex = 0;
+            // 
+            // selectionGroupbox
+            // 
+            this.selectionGroupbox.Controls.Add(this.finalScoreRadioButton);
+            this.selectionGroupbox.Controls.Add(this.findMidtermRadiobutton);
+            this.selectionGroupbox.Controls.Add(this.averageFinalsRadiobutton);
+            this.selectionGroupbox.Controls.Add(this.averageMidtermsRadioButton);
+            this.selectionGroupbox.Controls.Add(this.averageaAllTestsRadioButton);
+            this.selectionGroupbox.Location = new System.Drawing.Point(12, 12);
+            this.selectionGroupbox.Name = "selectionGroupbox";
+            this.selectionGroupbox.Size = new System.Drawing.Size(200, 143);
+            this.selectionGroupbox.TabIndex = 1;
+            this.selectionGroupbox.TabStop = false;
+            this.selectionGroupbox.Text = "groupBox1";
+            // 
+            // finalScoreRadioButton
+            // 
+            this.finalScoreRadioButton.AutoSize = true;
+            this.finalScoreRadioButton.Location = new System.Drawing.Point(6, 115);
+            this.finalScoreRadioButton.Name = "finalScoreRadioButton";
+            this.finalScoreRadioButton.Size = new System.Drawing.Size(101, 17);
+            this.finalScoreRadioButton.TabIndex = 8;
+            this.finalScoreRadioButton.TabStop = true;
+            this.finalScoreRadioButton.Text = "Find Final Score";
+            this.finalScoreRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // findMidtermRadiobutton
+            // 
+            this.findMidtermRadiobutton.AutoSize = true;
+            this.findMidtermRadiobutton.Location = new System.Drawing.Point(6, 92);
+            this.findMidtermRadiobutton.Name = "findMidtermRadiobutton";
+            this.findMidtermRadiobutton.Size = new System.Drawing.Size(116, 17);
+            this.findMidtermRadiobutton.TabIndex = 7;
+            this.findMidtermRadiobutton.TabStop = true;
+            this.findMidtermRadiobutton.Text = "Find Midterm Score";
+            this.findMidtermRadiobutton.UseVisualStyleBackColor = true;
+            // 
+            // averageFinalsRadiobutton
+            // 
+            this.averageFinalsRadiobutton.AutoSize = true;
+            this.averageFinalsRadiobutton.Location = new System.Drawing.Point(6, 69);
+            this.averageFinalsRadiobutton.Name = "averageFinalsRadiobutton";
+            this.averageFinalsRadiobutton.Size = new System.Drawing.Size(109, 17);
+            this.averageFinalsRadiobutton.TabIndex = 6;
+            this.averageFinalsRadiobutton.TabStop = true;
+            this.averageFinalsRadiobutton.Text = "Average Of Finals";
+            this.averageFinalsRadiobutton.UseVisualStyleBackColor = true;
+            // 
+            // averageMidtermsRadioButton
+            // 
+            this.averageMidtermsRadioButton.AutoSize = true;
+            this.averageMidtermsRadioButton.Location = new System.Drawing.Point(6, 46);
+            this.averageMidtermsRadioButton.Name = "averageMidtermsRadioButton";
+            this.averageMidtermsRadioButton.Size = new System.Drawing.Size(124, 17);
+            this.averageMidtermsRadioButton.TabIndex = 5;
+            this.averageMidtermsRadioButton.TabStop = true;
+            this.averageMidtermsRadioButton.Text = "Average Of Midterms";
+            this.averageMidtermsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // averageaAllTestsRadioButton
+            // 
+            this.averageaAllTestsRadioButton.AutoSize = true;
+            this.averageaAllTestsRadioButton.Location = new System.Drawing.Point(6, 23);
+            this.averageaAllTestsRadioButton.Name = "averageaAllTestsRadioButton";
+            this.averageaAllTestsRadioButton.Size = new System.Drawing.Size(122, 17);
+            this.averageaAllTestsRadioButton.TabIndex = 4;
+            this.averageaAllTestsRadioButton.TabStop = true;
+            this.averageaAllTestsRadioButton.Text = "Average Of All Tests";
+            this.averageaAllTestsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(227, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student Number:";
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(230, 121);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(88, 23);
+            this.displayButton.TabIndex = 3;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Selected Student\'s Scores:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Midterm:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(172, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Final:";
+            // 
+            // midtermLabel
+            // 
+            this.midtermLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.midtermLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.midtermLabel.Location = new System.Drawing.Point(77, 283);
+            this.midtermLabel.Name = "midtermLabel";
+            this.midtermLabel.Size = new System.Drawing.Size(76, 23);
+            this.midtermLabel.TabIndex = 7;
+            this.midtermLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // finalLabel
+            // 
+            this.finalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.finalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalLabel.Location = new System.Drawing.Point(220, 276);
+            this.finalLabel.Name = "finalLabel";
+            this.finalLabel.Size = new System.Drawing.Size(76, 23);
+            this.finalLabel.TabIndex = 7;
+            this.finalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(51, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Update Selected Student\'s Scores:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Midterm:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(174, 348);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Final:";
+            // 
+            // midtermTextbox
+            // 
+            this.midtermTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.midtermTextbox.Location = new System.Drawing.Point(77, 342);
+            this.midtermTextbox.Name = "midtermTextbox";
+            this.midtermTextbox.Size = new System.Drawing.Size(76, 22);
+            this.midtermTextbox.TabIndex = 8;
+            this.midtermTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // finalTextbox
+            // 
+            this.finalTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalTextbox.Location = new System.Drawing.Point(220, 342);
+            this.finalTextbox.Name = "finalTextbox";
+            this.finalTextbox.Size = new System.Drawing.Size(76, 22);
+            this.finalTextbox.TabIndex = 8;
+            this.finalTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // updateMidtermButton
+            // 
+            this.updateMidtermButton.Location = new System.Drawing.Point(63, 383);
+            this.updateMidtermButton.Name = "updateMidtermButton";
+            this.updateMidtermButton.Size = new System.Drawing.Size(100, 23);
+            this.updateMidtermButton.TabIndex = 9;
+            this.updateMidtermButton.Text = "Update Midterm";
+            this.updateMidtermButton.UseVisualStyleBackColor = true;
+            this.updateMidtermButton.Click += new System.EventHandler(this.updateMidtermButton_Click);
+            // 
+            // updateFinalButton
+            // 
+            this.updateFinalButton.Location = new System.Drawing.Point(200, 383);
+            this.updateFinalButton.Name = "updateFinalButton";
+            this.updateFinalButton.Size = new System.Drawing.Size(96, 23);
+            this.updateFinalButton.TabIndex = 9;
+            this.updateFinalButton.Text = "Update Final";
+            this.updateFinalButton.UseVisualStyleBackColor = true;
+            this.updateFinalButton.Click += new System.EventHandler(this.updateFinalButton_Click);
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageLabel.Location = new System.Drawing.Point(26, 172);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(281, 50);
+            this.averageLabel.TabIndex = 10;
+            this.averageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //this.averageLabel.Click += new System.EventHandler(this.averageLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 418);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.updateFinalButton);
+            this.Controls.Add(this.updateMidtermButton);
+            this.Controls.Add(this.finalTextbox);
+            this.Controls.Add(this.midtermTextbox);
+            this.Controls.Add(this.finalLabel);
+            this.Controls.Add(this.midtermLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectionGroupbox);
+            this.Controls.Add(this.studentNumberListbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.selectionGroupbox.ResumeLayout(false);
+            this.selectionGroupbox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox studentNumberListbox;
+        private System.Windows.Forms.GroupBox selectionGroupbox;
+        private System.Windows.Forms.RadioButton finalScoreRadioButton;
+        private System.Windows.Forms.RadioButton findMidtermRadiobutton;
+        private System.Windows.Forms.RadioButton averageFinalsRadiobutton;
+        private System.Windows.Forms.RadioButton averageMidtermsRadioButton;
+        private System.Windows.Forms.RadioButton averageaAllTestsRadioButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label midtermLabel;
+        private System.Windows.Forms.Label finalLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox midtermTextbox;
+        private System.Windows.Forms.TextBox finalTextbox;
+        private System.Windows.Forms.Button updateMidtermButton;
+        private System.Windows.Forms.Button updateFinalButton;
+        private System.Windows.Forms.Label averageLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+// Levi Martin 
+// 04/02/2020
+
+namespace TestScoreLookupandUpdateLeviMartin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // 2 dimmensional array to hold student grades
+        int[,] studentGradesArray = new int[10, 3];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //read file into the array and place first column in the list box
+                int row = 0;
+                string lineofdata;
+
+                //need a one dimmensional, 3 element string array for the split method
+                string[] splitlineofdata = new string[10];
+
+                StreamReader studentReader;
+                studentReader = File.OpenText("Grades.csv");
+
+                // loop through until end of file
+                while (!studentReader.EndOfStream)
+                {
+                    lineofdata = studentReader.ReadLine();
+                    splitlineofdata = lineofdata.Split(',');
+                    studentNumberListbox.Items.Add(splitlineofdata[0]);
+
+
+                    //organizing the data
+                    studentGradesArray[row, 0] = int.Parse(splitlineofdata[0]);
+                    studentGradesArray[row, 1] = int.Parse(splitlineofdata[1]);
+                    studentGradesArray[row, 2] = int.Parse(splitlineofdata[2]);
+
+                    
+                    row++;
+                }
+
+                studentReader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                //write array info to file
+                StreamWriter studentWriter;
+                studentWriter = File.CreateText("Grades.csv");
+                for (int row = 0; row < 10; row++)
+                {
+                    studentWriter.Write(studentGradesArray[row, 0] + ",");
+                    studentWriter.Write(studentGradesArray[row, 1] + ",");
+                    studentWriter.WriteLine(studentGradesArray[row, 2]);
+
+                }
+
+                studentWriter.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            //variables
+            decimal allTestAverage = 0; decimal midtermAverage; decimal finalAverage;
+            int allTestScores = 0; int midtermTestScores = 0; int finalTestScores = 0;
+
+
+            //radio button selections
+
+            if (averageaAllTestsRadioButton.Checked)
+            {
+                for (int row = 0; row < 10; row++)
+                {
+                    for (int col = 1; col < 3; col++)
+                    {
+                        // display average of all test scores held in array 
+                        allTestScores += studentGradesArray[row, col];
+                        allTestAverage = (decimal)allTestScores / 20m;
+                        averageLabel.Text = "Average of all tests is:" + "\n" + allTestAverage.ToString();
+
+                        midtermLabel.Text = "";
+                        finalLabel.Text = "";
+                    }
+                }
+            }
+            else
+            {
+                if (averageMidtermsRadioButton.Checked)
+                {
+                    for (int row = 0; row < 10; row++)
+                    {
+                        for (int col = 1; col < 2; col++)
+                        {
+                            // display average of all midterm test scores held in array 
+
+                            midtermTestScores += studentGradesArray[row, col];
+                            midtermAverage = (decimal)midtermTestScores / 10m;
+                            averageLabel.Text = "Average of midterms are:" + "\n" + midtermAverage.ToString();
+
+                            midtermLabel.Text = "";
+                            finalLabel.Text = "";
+                        }
+                    }
+                }
+                else if (averageFinalsRadiobutton.Checked)
+                {
+                    for (int row = 0; row < 10; row++)
+                    {
+                        for (int col = 2; col < 3; col++)
+                        {
+                            // display average of all finals test scores held in array 
+
+                            finalTestScores += studentGradesArray[row, col];
+                            finalAverage = (decimal)finalTestScores / 10m;
+                            averageLabel.Text = "Average of finals is:" + "\n" + finalAverage.ToString();
+
+                            midtermLabel.Text = "";
+                            finalLabel.Text = "";
+                        }
+                    }
+                }
+
+                else if (findMidtermRadiobutton.Checked)
+                {
+                    if (studentNumberListbox.SelectedIndex != -1)
+                    {
+                        // display student midterm test score 
+
+                        int row = studentNumberListbox.SelectedIndex;
+                        int col = 1;
+                        midtermTestScores = studentGradesArray[row, col];
+                        midtermLabel.Text = midtermTestScores.ToString();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a student ID number");
+                    }
+                }
+
+                else if (finalScoreRadioButton.Checked)
+                {
+                    if (studentNumberListbox.SelectedIndex != -1)
+                    {
+                        // display student final test score 
+
+                        int row = studentNumberListbox.SelectedIndex;
+                        int col = 2;
+                        midtermTestScores = studentGradesArray[row, col];
+                        finalLabel.Text = midtermTestScores.ToString();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select a student ID number");
+                    }
+                }
+            }
+        }
+
+        private void updateMidtermButton_Click(object sender, EventArgs e)
+        {
+            // student ID needs selected and score must be 0-100
+            if (studentNumberListbox.SelectedIndex != -1)
+            {
+                int row = studentNumberListbox.SelectedIndex;
+                int score;
+                bool isValid;
+                isValid = int.TryParse(midtermTextbox.Text, out score);
+                if (isValid && score >= 0 && score <= 100)
+                {
+                    studentGradesArray[row, 1] = score;
+                    MessageBox.Show("Midterm grade updated.");
+                    midtermTextbox.Clear();
+                    averageLabel.Text = "";
+                    midtermLabel.Text = score.ToString();
+
+                    finalLabel.Text = "";
+                }
+                else MessageBox.Show("Please enter a whole number 1-100");
+            }
+            else
+            {
+                MessageBox.Show("Please select a student ID");
+            }
+                     
+        }
+
+        private void updateFinalButton_Click(object sender, EventArgs e)
+        {
+            // student ID needs selected and score must be 0-100
+            if (studentNumberListbox.SelectedIndex != -1)
+            {
+                int row = studentNumberListbox.SelectedIndex;
+                int score;
+                bool isValid;
+                isValid = int.TryParse(finalTextbox.Text, out score);
+                if (isValid && score >= 0 && score <= 100)
+                {
+                    studentGradesArray[row, 2] = score;
+                    MessageBox.Show("Final grade updated.");
+                    finalTextbox.Clear();
+                    averageLabel.Text = "";
+                    finalLabel.Text = score.ToString();
+
+                    midtermLabel.Text = "";
+
+                }
+                else MessageBox.Show("Please enter a whole number 1-100");
+            }
+            else
+            {
+                MessageBox.Show("Please select a student ID");
+            }
+        }
+
+    }
+}
